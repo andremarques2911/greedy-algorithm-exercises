da4454dbef93972f36443c1dff2a13f4e2ffc763
André Marques <andre.marques@edu.pucrs.br>
2021-11-02 23:29:01 -0300

    adjustments in interval scale problem

--- src/main/java/IntervalScalingProblem.java
+++ src/main/java/IntervalScalingProblem.java
@@ -1,21 +1,39 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class IntervalScalingProblem {
 
     public static int iterations = 0;
 
-    public static List<Integer> execute(int[] s, int[] f, int n) {
-        List<Integer> x = new ArrayList<>();
+    public static int[] execute(int[] s, int[] f, int n) {
+        sort(s, f);
+        int[] x = new int[n];
         int i = 0;
-        for (int k = 0; k < n; k++) {
-            if (s[k] > f[i] && !x.contains(s[k])) {
-                x.add(s[k]);
+        for (int k = 1; k < n; k++) {
+            if (s[k] >= f[i]) {
+                x[i + 1] = 1;
                 i = k;
             }
             iterations++;
         }
         return x;
+    }
+
+    private static void sort(int[] s, int[] f) {
+        boolean changed;
+        do {
+            changed = false;
+            for (var i=0; i < s.length - 1; i++) {
+                if (f[i] > f[i+1]) {
+                    f[i] = f[i] ^ f[i+1];
+                    f[i+1] = f[i+1] ^ f[i];
+                    f[i] = f[i] ^ f[i+1];
+
+                    s[i] = s[i] ^ s[i+1];
+                    s[i+1] = s[i+1] ^ s[i];
+                    s[i] = s[i] ^ s[i+1];
+
+                    changed = true;
+                }
+            }
+        } while (changed);
     }
 
 }
--- src/test/java/GreedyAlgorithmsTests.java
+++ src/test/java/GreedyAlgorithmsTests.java
@@ -1,7 +1,5 @@
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
 
 public class GreedyAlgorithmsTests {
 
@@ -10,9 +8,9 @@ public class GreedyAlgorithmsTests {
         ChangeProblem changeProblem = new ChangeProblem(new int[]{ 100, 25, 10, 5, 1 });
         var res = changeProblem.execute(2.89);
         var expected = new int[]{ 2, 3, 1, 0, 4 };
-        Assertions.assertEquals(expected.length, res.length);
+        assertEquals(expected.length, res.length);
         for (int i = 0; i < expected.length; i++)
-            Assertions.assertEquals(expected[i], res[i]);
+            assertEquals(expected[i], res[i]);
         printIterations(changeProblem.iterations);
     }
 
@@ -20,7 +18,11 @@ public class GreedyAlgorithmsTests {
     public void IntervalScalingProblem() {
         int[] s = { 4, 6, 13, 4, 2, 6, 7,  9,  1, 3,  9 };
         int[] f = { 8, 7, 14, 5, 4, 9, 10, 11, 6, 13, 12 };
+        int[] expected = { 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0 };
         var res = IntervalScalingProblem.execute(s, f, 11);
+        assertEquals(expected.length, res.length);
+        for (int i = 0; i < expected.length; i++)
+            assertEquals(expected[i], res[i]);
         printIterations(IntervalScalingProblem.iterations);
     }
 
@@ -28,6 +30,7 @@ public class GreedyAlgorithmsTests {
     public void NQueensProblem() {
         NQueensProblem nQueensProblem = new NQueensProblem(7);
         var res = nQueensProblem.execute();
+        assertEquals(7, res.size());
         printIterations(nQueensProblem.iterations);
     }
 
